1220d4be39bf3e2f05ab42fb3fca00e3a06bcf4e
Ujjwal Singh <78536380+madmaxujjwal@users.noreply.github.com>
2022-07-28 23:30:06 +0600

    Time: 48 ms (73.74%), Space: 13.4 MB (51.71%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,40 +1,36 @@
-class Solution {
-public:
-    int maxPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        sort(points.begin(),points.end());
-        vector<vector<int>> p;
-        p.push_back(points[0]);
-        for(int i=1;i<n;i++){
-            vector<int> a=points[i];
-            if(points[i]!=points[i-1]){
-                p.push_back(a);
-            }
-        }
-        unordered_map<double,double> ht;
-        long double l=0;double z=0;
-        for(int i=0;i<n;i++){
-          
-            for(int j=0;j<n;j++){
-                if(i!=j){
-                    double m;
-                    if(p[i][0]-p[j][0]!=0)
-                     m=((double)p[i][1]-(double)p[j][1])/((double)p[i][0]-(double)p[j][0]);
+class Solution
+{
+    public:
+        int maxPoints(vector<vector < int>> &points)
+        {
+            int n = points.size();
+            if(n<=2) return n;
+            
+            int max_point = 0;
+            double slope = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                unordered_map<double, int> mp;
+                for (int j = i + 1; j < n; j++)
+                {
+                    int y2 = points[j][1];
+                    int y1 = points[i][1];
+
+                    int x2 = points[j][0];
+                    int x1 = points[i][0];
+
+                    if(x2==x1)
+                        slope=INT_MAX;
                     else
-                      m=INT_MAX;
-                     ht[m]++;
-                    z=max(z,ht[m]);
+                      slope = (double)(y2 - y1) / (double)(x2 - x1);
+                    mp[slope]++;
+					
                 }
+                for (auto slope: mp)
+					max_point = max(max_point, slope.second);
                
             }
-           
-            if(z>l){
-                l=z;
-            }
-            ht.clear();
-            z=0;
-        }
-        
-        return l+1;
-    }
+            return max_point + 1;
+       }
 };
