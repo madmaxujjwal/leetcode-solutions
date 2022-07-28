b6af1b6a99c22848808792d6f288ddf04fb7c455
Ujjwal Singh <78536380+madmaxujjwal@users.noreply.github.com>
2022-07-28 23:46:38 +0600

    Time: 23 ms (98.39%), Space: 11.3 MB (80.01%) - LeetHub

--- 149-max-points-on-a-line/149-max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/149-max-points-on-a-line.cpp
@@ -1,36 +1,18 @@
-class Solution
-{
-    public:
-        int maxPoints(vector<vector < int>> &points)
-        {
-            int n = points.size();
-            if(n<=2) return n;
-            
-            int max_point = 0;
-            double slope = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                unordered_map<double, int> mp;
-                for (int j = i + 1; j < n; j++)
-                {
-                    int y2 = points[j][1];
-                    int y1 = points[i][1];
-
-                    int x2 = points[j][0];
-                    int x1 = points[i][0];
-
-                    if(x2==x1)
-                        slope=INT_MAX;
-                    else
-                      slope = (double)(y2 - y1) / (double)(x2 - x1);
-                    mp[slope]++;
-					
-                }
-                for (auto slope: mp)
-					max_point = max(max_point, slope.second);
-               
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& p) {
+        sort(p.begin(),p.end());
+        unordered_map<double,int>m;
+        int ans = 0;
+        for(int i = 0;i<p.size();i++){
+            for(int j = i+1;j<p.size();j++){
+            double tan = (p[i][1]-p[j][1])/double(p[i][0]-p[j][0]);
+            m[tan]++;
+            ans = max(ans,m[tan]);
             }
-            return max_point + 1;
-       }
+            m.clear();
+        }
+        return  ans+1;
+    }
+    
 };
