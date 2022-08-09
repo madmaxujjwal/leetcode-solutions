5ab460a7425a00742ac00763acfe367d565be36c
Ujjwal Singh <78536380+madmaxujjwal@users.noreply.github.com>
2022-08-10 00:10:16 +0600

    Time: 7 ms (7.85%), Space: 5.9 MB (65.70%) - LeetHub

---
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isThree(int n) {
+        int c=0;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                c++;
+            }
+            
+        }
+        return c==1;
+        
+    }
+};
